4261b1e87c95201cc74d75e6530b3b6bb9f9b966
Tom <thomas.berghuis@gmail.com>
2019-06-16 16:42:47 +1000

    .

--- src/screens/DateTimeTest.js
+++ src/screens/DateTimeTest.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
-import { Button, View } from "react-native";
+import { Button, View, Text } from "react-native";
 import DateTimePicker from "react-native-modal-datetime-picker";
 
 import { observable } from "mobx";
 import { observer } from "mobx-react";
 
+import setMinutes from "date-fns/set_minutes";
+import setHours from "date-fns/set_hours";
+// import getMinutes from "date-fns/get_minutes";
+// import getHours from "date-fns/get_hours";
+import format from "date-fns/format";
+
+var tmpdate = new Date();
+tmpdate = setHours(tmpdate, 7);
+tmpdate = setMinutes(tmpdate, 0);
+
 const localState = observable({
-  isDateTimePickerVisible: false
+  isDateTimePickerVisible: false,
+  // do it wrong
+  dailyNotificationDate: tmpdate
 });
 
 export default
@@ -22,17 +34,28 @@ class DateTimePickerTester extends Component {
 
   handleDatePicked = date => {
     console.log("A date has been picked: ", date);
+
+    // localState.dailyNotificationTimeHours = getHours(date);
+    // localState.dailyNotificationTimeMinutes = getMinutes(date);
+
+    localState.dailyNotificationDate = date;
+
     this.hideDateTimePicker();
   };
 
   render() {
     return (
       <View>
-        <Button title="Show DatePicker" onPress={this.showDateTimePicker} />
+        
+        <Text>Daily reminder time: {format(localState.dailyNotificationDate, "HH:mm")}</Text>
+        <Button title="Set reminder time" onPress={this.showDateTimePicker} />
         <DateTimePicker
           isVisible={localState.isDateTimePickerVisible}
           onConfirm={this.handleDatePicked}
           onCancel={this.hideDateTimePicker}
+          // datePickerModeAndroid="spinner"
+          mode="time"
+          date={localState.dailyNotificationDate}
         />
       </View>
     );
